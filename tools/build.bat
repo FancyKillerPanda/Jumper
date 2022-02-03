c92f66f774186dc155f6e17ba32328d598af23a2
FancyKillerPanda <fancykillerpanda@gmail.com>
2022-02-03 16:04:38 +1100

    Windows build file can now build debug and release.

--- tools/build.bat
+++ tools/build.bat
@@ -3,10 +3,32 @@
 set scriptDir=%~dp0
 set projectRoot=%scriptDir%\..
 
-mkdir %projectRoot%\bin\ >nul 2>nul
-cd %projectRoot%\bin\
+if [%~1]==[--release] (
+	mkdir %projectRoot%\bin\release\ >nul 2>nul
+	cd %projectRoot%\bin\release\
 
-xcopy %projectRoot%\res\ %projectRoot%\bin\res\ /s /e
-odin run %projectRoot%\src\ -opt:0 -out:jumper.exe -thread-count:8 -debug
+	xcopy %projectRoot%\res\ %projectRoot%\bin\release\res\ /s /e /y /q
+	xcopy "C:\Program Files\Odin\vendor\sdl2\SDL2.dll" %projectRoot%\bin\release\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\SDL2_image.dll" %projectRoot%\bin\release\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\libpng16-16.dll" %projectRoot%\bin\release\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\zlib1.dll" %projectRoot%\bin\release\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\ttf\SDL2_ttf.dll" %projectRoot%\bin\release\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\ttf\libfreetype-6.dll" %projectRoot%\bin\release\ /q /y
+	
+	odin run %projectRoot%\src\ -o:size -out:jumper.exe -thread-count:8
+) else (
+	mkdir %projectRoot%\bin\debug\ >nul 2>nul
+	cd %projectRoot%\bin\debug\
+
+	xcopy %projectRoot%\res\ %projectRoot%\bin\debug\res\ /s /e /y /q
+	xcopy "C:\Program Files\Odin\vendor\sdl2\SDL2.dll" %projectRoot%\bin\debug\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\SDL2_image.dll" %projectRoot%\bin\debug\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\libpng16-16.dll" %projectRoot%\bin\debug\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\image\zlib1.dll" %projectRoot%\bin\debug\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\ttf\SDL2_ttf.dll" %projectRoot%\bin\debug\ /q /y
+	xcopy "C:\Program Files\Odin\vendor\sdl2\ttf\libfreetype-6.dll" %projectRoot%\bin\debug\ /q /y
+	
+	odin run %projectRoot%\src\ -opt:0 -out:jumper.exe -thread-count:8 -debug
+)
 
 cd %projectRoot%
