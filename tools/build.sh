e4a1786ca4b53066c4c55f2d90f2bc711082a1e9
FancyKillerPanda <fancykillerpanda@gmail.com>
2022-02-03 16:06:32 +1100

    Linux build file can now build both debug and release.

--- tools/build.sh
+++ tools/build.sh
@@ -3,10 +3,18 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 projectRoot=$scriptDir/..
 
-mkdir $projectRoot/bin/ 2>/dev/null
-cd $projectRoot/bin/
+if [[ "$1" == "--release" ]]; then
+	mkdir -p $projectRoot/bin/release/ 2>/dev/null
+	cd $projectRoot/bin/release/
 
-cp -r $projectRoot/res/ $projectRoot/bin/
-odin run $projectRoot/src/ -opt:0 -out:jumper -thread-count:8 -debug
+	cp -r $projectRoot/res/ $projectRoot/bin/release/
+	odin run $projectRoot/src/ -o:size -out:jumper -thread-count:8
+else
+	mkdir -p $projectRoot/bin/debug/ 2>/dev/null
+	cd $projectRoot/bin/debug/
+
+	cp -r $projectRoot/res/ $projectRoot/bin/debug/
+	odin run $projectRoot/src/ -opt:0 -out:jumper -thread-count:8 -debug
+fi
 
 cd $projectRoot
